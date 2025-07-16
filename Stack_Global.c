a426c8696fae55a7124d23de842956d807d0d0a3
Arj2001 <120589180+jyothimagimary@users.noreply.github.com>
2025-07-16 14:59:08 +0530

    New commmit

---
+++ Stack_Global.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+int top=-1;
+int stack[10];
+
+void push(int el){
+	if(top==9){
+		printf("Error: Stack Overflow");
+	}
+	else{
+		stack[++top]=el;
+		printf("%d pushed into stack",el);
+	}
+}
+
+int pop(){
+	if(top==-1){
+		printf("Error: Stack Underflow");
+	}
+	else{
+		return stack[top--];
+	}
+}
+
+void peek(){
+	if(top==-1){
+			printf("Error: Stack Empty");
+	}
+	else{
+		printf("%d",stack[top]);
+	}
+}
+
+void display(){
+	int i;
+	if(top==-1){
+		printf("Error: Stack Empty");
+	}
+	else{
+		for(i=0;i<=top;i++){
+			printf("%d\t",stack[i]);
+		}
+	}
+}
+
+int main(){
+	int ch=1,el;
+	while(ch){
+		printf("\n0.Exit\n1.PUSH\n2.POP\n3.PEEK\n4.Display\nSelect one operation: ");
+		scanf("%d",&ch);
+		switch(ch){
+			case 0:return;break;
+			case 1:printf("Enter element to push: ");
+				scanf("%d",&el);
+				push(el);
+				break;
+		}
+	}
+}
